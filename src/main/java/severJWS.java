4022ca3a9c24a1fc6a1967407d30ce19e5fa2f6d
moukh-tari <mmoukhtari57@gmail.com>
2023-06-15 00:39:36 +0100

    tp1

---
+++ src/main/java/severJWS.java
@@ -0,0 +1,12 @@
+
+import jakarta.xml.ws.Endpoint;
+import ws.BanqueService;
+
+public class severJWS {
+    public static void main (String[] args) {
+        Endpoint.publish("http://0.0.0.0:9191/", new BanqueService());
+        System.out.println("Web service déployé sur http://0.0.0.0:9191/**");
+    }
+
+
+    }
